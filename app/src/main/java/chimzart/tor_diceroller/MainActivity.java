17beab63e765d2aa47d329fabaa222b7cd1fef08
Fernando Ferreira <fernando.ferreira@ufrgs.br>
2017-10-09 21:27:55 -0300

    Initial framework.
     -User's interface;
     -Functional OnClick button dialog;
     -Load screen;

--- app/src/main/java/chimzart/tor_diceroller/MainActivity.java
+++ app/src/main/java/chimzart/tor_diceroller/MainActivity.java
@@ -3,20 +3,50 @@ package chimzart.tor_diceroller;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar_dados;
+    private Switch switch_vantage;
+    private Switch switch_weary;
+    private Button button_roll;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        seekBar_dados = (SeekBar) findViewById(R.id.seekBar_dados);
+        switch_vantage = (Switch) findViewById(R.id.switch_vantage);
+        switch_weary = (Switch) findViewById(R.id.switch_weary);
+        button_roll = (Button) findViewById(R.id.button_roll);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button_roll.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage(button_roll.getText().toString());
+                dlg.setNeutralButton("OK",null);
+                dlg.show();
+
+            }
+
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
