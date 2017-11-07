f899c66f6aff17ce296562b7aa264d00a7f47a03
Fernando Ferreira <fernando.ferreira@ufrgs.br>
2017-11-06 23:56:19 -0200

    Bug Fix: -none
      Added Features:
    
      -Advantage/Disadvantage function
      -No Feat die routine
      - Switches interlock
     - drawable dice resources.

--- app/src/main/java/chimzart/tor_diceroller/MainActivity.java
+++ app/src/main/java/chimzart/tor_diceroller/MainActivity.java
@@ -8,18 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.*;
 import android.widget.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
 // componentes interface
     private SeekBar seekBar_dices;
-    private Switch switch_vantage;
+    private Switch switch_disadvantage;
+    private Switch switch_advantage;
+    private Switch switch_feat;
     private Switch switch_weary;
     private Button button_roll;
     private TextView seekBar_progress;
     private int diceValue = 0;
-    //private boolean vantage_Status = false;
-    //private int disvantage_Status = false;
+    //private boolean advantage_Status = false;
+    //private int disadvantage_Status = false;
     //private int weary_Status = false;
 
 // cria a interface
@@ -32,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar_dices = (SeekBar) findViewById(R.id.seekBar_dices);
         seekBar_progress = (TextView) findViewById(R.id.seekBar_progress);
-        switch_vantage = (Switch) findViewById(R.id.switch_vantage);
+        switch_feat = (Switch) findViewById(R.id.switch_feat);
         switch_weary = (Switch) findViewById(R.id.switch_weary);
+        switch_advantage = (Switch) findViewById(R.id.switch_advantage);
+        switch_disadvantage = (Switch) findViewById(R.id.switch_disadvantage);
         button_roll = (Button) findViewById(R.id.button_roll);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -58,7 +64,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//Switches
 
+        switch_feat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switch_advantage.setEnabled(false);
+                    switch_disadvantage.setEnabled(false);
+                } else {
+                    switch_advantage.setEnabled(true);
+                    switch_disadvantage.setEnabled(true);
+                }
+            }
+        });
+
+        switch_advantage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switch_disadvantage.setEnabled(false);
+                } else {
+                    switch_disadvantage.setEnabled(true);
+                }
+            }
+        });
+
+        switch_disadvantage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switch_advantage.setEnabled(false);
+                } else {
+                    switch_advantage.setEnabled(true);
+                }
+            }
+        });
 //Button
         button_roll.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -77,17 +118,36 @@ public class MainActivity extends AppCompatActivity {
 
                 }
                 //account for great and extraordinary sucess
-                //roll the feat die
-                int Roll = diceRoll(12);
-                //check weary status and ignore the lessers results
-                ResString += " |Feat Die:";
-                if(switch_weary.isChecked() && Roll<= 3 ){
-                    ResString += ": NULO";
-                }else{
-                    ResString += ": " +Roll;}
 
-                //roll again for vantage/disvantage plays
-                //check for Gandalf's rune and Sauron's eye
+
+                //roll the feat die
+                if(!switch_feat.isChecked()) {
+
+                    //roll again for vantage/disvantage plays
+                    Integer[] RollAr = new Integer[] {0,0};
+                    RollAr[0] = diceRoll(12);
+                    RollAr[1] = diceRoll(12);
+
+                    if(switch_advantage.isChecked()) {
+                        Arrays.sort(RollAr, Collections.reverseOrder());
+                    }
+                    if(switch_disadvantage.isChecked()) {
+                        Arrays.sort(RollAr);
+
+
+                    }
+
+                    //check weary status and ignore the lessers results
+                    ResString += " |Feat Die:";
+                    if (switch_weary.isChecked() && RollAr[0] <= 3) {
+                        ResString += ": NULO";
+                    } else {
+                        ResString += ": " + RollAr[0];
+                    }
+
+
+                    //check for Gandalf's rune and Sauron's eye
+                }
 
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
                 dlg.setMessage(ResString);
